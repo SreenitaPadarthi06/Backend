2d7e485885942a8a7b657f2e36f2d25323ae4813
SreenitaPadarthi06 <sreenitapadarthi@gmail.com>
2025-12-06 12:14:11 +0530

    Middleware

---
+++ middleware.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app=express();
+app.use(express.json());
+app.get('/get-user',(req,res)=>{
+    res.send("api success");
+})
+app.post('/add-user',(req,res)=>{
+    let data=req.body;
+    console.log(data);
+    res.send("data added");
+});
+
+app.put('/put-user',(req,res)=>{
+    let data=req.body;
+    console.log(data);
+    res.send("Request accepted");
+});
+app.delete('/delete-user',(req,res)=>{
+    let data=req.body;
+    console.log(data);
+    res.send("Deleted Successfully");
+});
+app.listen(9000,()=>{
+    console.log("Listen");
+});
+
